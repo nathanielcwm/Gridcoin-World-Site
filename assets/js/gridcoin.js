ad9c119b0f02acfb023796d19d75e5382c587500
nathanielcwm <22572406+nathanielcwm@users.noreply.github.com>
2023-07-11 10:35:53 +0800

    Port current downloads script from .us

--- assets/js/gridcoin.js
+++ assets/js/gridcoin.js
@@ -1,27 +1,16 @@
+const allPlatforms = ["win64", "win32", "macos_arm", "macos"];
+var platformsMissed = [...allPlatforms];
+
+function show(element) {
+  element.style.display = "";
+}
+
 function onScroll() {
     if ($(document).scrollTop() > 50) {
         $(".navbar").addClass("navbar-scroll");
     } else {
         $(".navbar").removeClass("navbar-scroll");
     }
-}
-
-function getLatestRelease() {
-    var walletVersion = $('#wallet-version');
-
-    if (!walletVersion.length) {
-        return;
-    }
-
-    $.ajax({
-        type: 'GET',
-        url:'https://api.github.com/repos/gridcoin-community/Gridcoin-Research/releases/latest',
-        crossDomain: true,
-        dataType: 'json',
-        success: function(data) {
-            walletVersion.text('Current Wallet Version: ' + data['name']);
-        }
-    });
 }
 
 /**
@@ -61,14 +50,108 @@ function scrollToTheTab(target) {
   }, 50, 'swing');
 }
 
-$(document).ready(function () {
-    onScroll();
-    $(document).on("scroll", onScroll);
-    $('[data-toggle="tooltip"]').tooltip() //Enabled bootstrap tooltips.
+// $(document).ready(function () {
+//     onScroll();
+//     $(document).on("scroll", onScroll);
 
-    getLatestRelease();
+//     initTabs();
+//     handleTabSwitch();
+//     $(window).bind('hashchange', handleTabSwitch);
+// });
 
-    initTabs();
-    handleTabSwitch();
-    $(window).bind('hashchange', handleTabSwitch);
-});
+//Enable bootstrap tooltips.
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
+
+function doOtherPlatformsMatch(targetPlatform, name) {
+
+  for (let comparePlatform of allPlatforms) {
+
+    if (comparePlatform.length >= targetPlatform.length // don't match macos for macos_arm but do the other way around 
+      && comparePlatform !== targetPlatform
+      && name.includes(targetPlatform)
+      && name.includes(comparePlatform)
+    ) {
+      return true;
+    }
+
+  }
+
+  return false;
+}
+
+function updateDownloads(data, platforms = allPlatforms, previousVersion = false) {
+
+  if (!previousVersion) {
+    document.getElementById("wallet-version").textContent = 'Current Wallet Version: ' + data.name;
+  }
+
+  let hotfixes = [];
+
+  for (let assetFile of data.assets) {
+
+    const name = assetFile.name;
+    const downloadURL = assetFile.browser_download_url;
+
+    if (name.includes(".SHA256")) {
+      continue;
+    }
+
+    for (let platform of platforms) {
+      if (name.includes(platform) && !doOtherPlatformsMatch(platform, name)) {
+
+        if (name.includes("hotfix")) {
+          hotfixes.push(platform);
+        } else if (hotfixes.includes(platform)) {
+          break; //if a hotfix already exists, don't update the file
+        }
+
+        const platformButton = document.getElementById(platform);
+        platformButton.href = downloadURL;
+        show(platformButton);
+
+        if (name.includes("min")) {
+          const startOfMinVersion = name.indexOf("min-") + 4;
+          const endOfFilename = name.lastIndexOf(".");
+          const minVersion = name.slice(startOfMinVersion, endOfFilename);
+
+          platformButton.textContent += " (min OS: " + minVersion + ")";
+        }
+
+        if (previousVersion) {
+          const versionWarn = document.getElementById(platform + "-version-warn");
+          versionWarn.textContent += data.name;
+          show(versionWarn);
+        }
+
+        platformsMissed = platformsMissed.filter(value => value !== platform); //remove platform from missed list
+
+        break;
+      }
+    }
+  }
+}
+
+function fillInMissing() {
+  if (platformsMissed.length > 0) {
+    fetch("https://api.github.com/repos/gridcoin-community/Gridcoin-Research/releases")
+      .then(releases => releases.json())
+      .then(releases => {
+        for (let release of releases) {
+          updateDownloads(release, platformsMissed, true);
+          if (platformsMissed.length === 0) {
+            break;
+          }
+        }
+      })
+  }
+}
+
+if (document.getElementById("wallet-version")) { //only get version data on pages where version number and download links are needed
+  fetch("https://api.github.com/repos/gridcoin-community/Gridcoin-Research/releases/latest")
+    .then(response => response.json())
+    .then(updateDownloads)
+    .then(fillInMissing)
+}
